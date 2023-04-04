377578dd3a04b935365380014f647cbccd187685
Levid <jassontejada@gmail.com>
2023-04-04 13:13:20 -0400

    Changing error msg

--- Models/Seller.cs
+++ Models/Seller.cs
@@ -6,16 +6,16 @@ public class Seller
     [Required(ErrorMessage = "El nombre es obligatorio.")]
     [StringLength(100, MinimumLength = 2, ErrorMessage = "El nombre debe tener {2} o más dígitos.")]
     public string Name { get; set; } = string.Empty;
-    [Required(ErrorMessage = "El APELLIDO es obligatorio.")]
+    [Required(ErrorMessage = "El apellido es obligatorio.")]
     [StringLength(100, MinimumLength = 2, ErrorMessage = "El APELLIDO es demasiado corto, Debe tener al menos {2} dígitos")]
     public string LastName { get; set; } = string.Empty;
     [Required(ErrorMessage = "El número de identificación es obligatorio.")]
     [RegularExpression("^[0-9]*$", ErrorMessage = "El número de identificación debe contener solo numeros.")]
     public string IdentificationNumber { get; set; } = string.Empty;
-    [Required(ErrorMessage = "La DIRECCIÓN es obligatorio.")]
+    [Required(ErrorMessage = "La direcció es obligatorio.")]
     [StringLength(500, MinimumLength = 5, ErrorMessage = "Debe introducir una direccion real.")]
     public string Address { get; set; } = string.Empty;
-    [Required(ErrorMessage = "El campo telefono es obligatorio.")]
+    [Required(ErrorMessage = "El campo teléfono es obligatorio.")]
     [RegularExpression("^[0-9]*$", ErrorMessage = "El telefono debe contener solo numeros.")]
     [StringLength(12, MinimumLength = 10, ErrorMessage = "El telefono debe contener al menos {2} dígitos y maximo {1} dígitos.")]
     public string Telephone { get; set; } = string.Empty;
