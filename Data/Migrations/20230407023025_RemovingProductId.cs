484df116a0f5404560601e2fcca3efe333c6f418
Levid <jassontejada@gmail.com>
2023-04-07 00:21:21 -0400

    migration for Sale->ProducitId

---
+++ Data/Migrations/20230407023025_RemovingProductId.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SalesSystem_AP1_Levid.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class RemovingProductId : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ProductId",
+                table: "Sales");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "ProductId",
+                table: "Sales",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: 0);
+        }
+    }
+}
