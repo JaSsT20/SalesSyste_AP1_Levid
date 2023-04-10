14546f6bad9af4ab7c72a11fce17e4a2f1698274
Levid <jassontejada@gmail.com>
2023-04-09 22:45:26 -0400

    Updating sellers

--- Models/Seller.cs
+++ Models/Seller.cs
@@ -5,19 +5,21 @@ public class Seller
     public int SellerId { get; set; }
     [Required(ErrorMessage = "El nombre es obligatorio.")]
     [StringLength(100, MinimumLength = 2, ErrorMessage = "El nombre debe tener {2} o más dígitos.")]
+    [RegularExpression(@"^[^\d]+$", ErrorMessage = "El nombre no puede contener números.")]
     public string Name { get; set; } = string.Empty;
     [Required(ErrorMessage = "El apellido es obligatorio.")]
-    [StringLength(100, MinimumLength = 2, ErrorMessage = "El APELLIDO es demasiado corto, Debe tener al menos {2} dígitos")]
+    [StringLength(100, MinimumLength = 2, ErrorMessage = "El apellido es demasiado corto, debe tener al menos {2} dígitos")]
+    [RegularExpression(@"^[^\d]+$", ErrorMessage = "El apellido no puede contener números.")]
     public string LastName { get; set; } = string.Empty;
     [Required(ErrorMessage = "El número de identificación es obligatorio.")]
     [RegularExpression("^[0-9]*$", ErrorMessage = "El número de identificación debe contener solo numeros.")]
     public string IdentificationNumber { get; set; } = string.Empty;
-    [Required(ErrorMessage = "La direcció es obligatorio.")]
+    [Required(ErrorMessage = "La dirección es obligatoria.")]
     [StringLength(500, MinimumLength = 5, ErrorMessage = "Debe introducir una direccion real.")]
     public string Address { get; set; } = string.Empty;
     [Required(ErrorMessage = "El campo teléfono es obligatorio.")]
-    [RegularExpression("^[0-9]*$", ErrorMessage = "El telefono debe contener solo numeros.")]
-    [StringLength(12, MinimumLength = 10, ErrorMessage = "El telefono debe contener al menos {2} dígitos y maximo {1} dígitos.")]
+    [RegularExpression("^[0-9]*$", ErrorMessage = "El teléfono debe contener solo números.")]
+    [StringLength(12, MinimumLength = 10, ErrorMessage = "El teléfono debe contener al menos {2} dígitos y maximo {1} dígitos.")]
     public string Telephone { get; set; } = string.Empty;
     public DateTime EntryDate { get; set; } = DateTime.Today;
     public float TotalSold { get; set; }
